86340a3c0862bd608ce866d9b6a006df26d37cbc
jeffero <jheremenis@gmail.com>
2019-12-02 10:41:42 +0100

    first version

---
+++ assets/js/main.js
@@ -0,0 +1,106 @@
+const {clipboard, electron} = require('electron');
+require('electron').ipcRenderer.on('uploadedFile', function(event, message) {
+	message = JSON.parse(message);
+
+	createUploadedFile(message, {name: message.name});
+})
+
+$('body').on('dragover', function(e) {
+	e.preventDefault();
+	e.stopPropagation();
+});
+
+$('body').on('dragenter', function(e) {
+	e.preventDefault();
+	e.stopPropagation();
+});
+
+$('body').on('drop', function(e){
+	if (e.originalEvent.dataTransfer && e.originalEvent.dataTransfer.files.length) {
+		e.preventDefault();
+		e.stopPropagation();
+		/*UPLOAD FILES HERE*/
+		uploadFiles(e.originalEvent.dataTransfer.files);
+	}
+});
+
+$('.upload-btn-wrapper .btn').click(() => {$('#uploadFile').click()});
+$('#uploadFile').on('change', function() {
+	if ($(this)[0].files) {
+		uploadFiles($(this)[0].files);
+	}
+});
+
+function uploadFiles(files) {
+	for (var index in files) {
+		var file = files[index];
+		uploadFile(file);
+	}
+}
+
+function uploadFile(file) {
+	if (typeof file != 'object') return;
+	var formData = new FormData();
+	formData.append('file', file);
+
+	$.ajax({
+		url : 'https://pixeldrain.com/api/file',
+		type : 'POST',
+		data : formData,
+		processData: false,  // tell jQuery not to process the data
+		contentType: false,  // tell jQuery not to set contentType
+		success : function(data) {
+			const lastUrl = 'https://pixeldrain.com/u/'+data.id;
+			clipboard.writeText(lastUrl);
+			createUploadedFile(data, file);
+		}
+	});
+}
+
+function createUploadedFile(data, file) {
+	file = file || {};
+	const date = new Date();
+	const fileName = file.name || data.id;
+	const lastUrl = 'https://pixeldrain.com/u/'+data.id;
+	const thumbnail = 'https://pixeldrain.com/api/file/'+data.id+'/thumbnail';
+	const time = +date.getHours()+':'+pad(date.getMinutes())
+
+	const prepender = `
+	<div class="col-md-4">
+		<div class="card mb-4 shadow-sm">
+			<img class="bd-placeholder-img card-img-top" width="100%" height="225" src="${thumbnail}"/>
+			<div class="card-body">
+				<p class="card-text">${fileName}</p>
+				<div class="d-flex justify-content-between align-items-center">
+					<div class="btn-group">
+						<a onclick="newBrowserWindow('${lastUrl}')" oldonclick="window.open('${lastUrl}', '_blank', 'nodeIntegration=no')" class="btn btn-sm btn-outline-secondary" target="_BLANK">View</a>
+						<button type="button" class="btn btn-sm btn-outline-secondary" onclick="clipboard.writeText(\'${lastUrl}\');">Copy</button>
+					</div>
+					<small class="text-muted">${time}</small>
+				</div>
+			</div>
+		</div>
+	</div>
+	`;
+
+	$('#uploaded-files').prepend(prepender);
+}
+
+function pad(digit) {	return digit < 10 ? '0'+digit:digit; }
+
+
+function newBrowserWindow(url, options) {
+	const { BrowserWindow } = require('electron').remote;
+	let wind = new BrowserWindow({
+		width: 1200,
+		height: 800,
+		backgroundColor: '#312450',
+		frame: false,
+		// alwaysOnTop: true,
+		webPreferences: {
+			nodeIntegration: false
+		}
+	});	
+	wind.loadURL(url);
+
+}
---
+++ main.js
@@ -0,0 +1,156 @@
+const { app, clipboard, BrowserWindow, Menu, Tray, globalShortcut} = require('electron')
+const screenshot = require('screenshot-desktop')
+const request = require('request');
+var screenshotNode = require('screenshot-node');
+const {openCropperWindow, isCropperOpen} = require('./modules/cropper')
+// const Store = require('electron-store');
+// const store = new Store();
+
+
+const uploadUrl = 'https://pixeldrain.com/api/file';
+let win = null;
+let tray = null;
+
+function onReady() {
+	// fillingTheShelves();
+	createTray();
+	addListeners();
+	createWindow();
+}
+
+function fillingTheShelves() {
+	if (store.get('playSound')) { store.set('playSound', true) }
+	if (store.get('alwaysOnTop') != undefined) {	store.set('alwaysOnTop', false)}
+}
+
+function createTray() {
+	tray = new Tray('assets/images/icon.ico');
+	const contextMenu = Menu.buildFromTemplate([
+		{label: 'Settings', type: 'radio', click: openWindow},
+		{label: 'Exit', type: 'radio', click: app.quit}
+	]);
+	tray.setToolTip('PixelPour!');
+	tray.setContextMenu(contextMenu);
+	tray.on('double-click', openWindow);
+}
+
+function openWindow () {
+	if (win) {
+		if (win.isMinimized()) {
+			win.restore();
+		}
+		win.focus();
+	} else {
+		createWindow();
+	}
+}
+
+function createWindow() {
+	// Create the browser window.
+	win = new BrowserWindow({
+		width: 1200,
+		height: 800,
+		backgroundColor: '#0d0d0d',
+		// frame: false,
+		icon: 'assets/images/icon.ico',
+		// alwaysOnTop: true,
+		autoHideMenuBar: true,
+		webPreferences: {
+			nodeIntegration: true
+		}
+	});
+
+	// and load the index.html of the app.
+  win.loadFile('index.html')
+}
+
+async function addListeners() {
+	var displays = await screenshot.listDisplays();
+	// Fuck man, this ain't really pretty huh
+	
+	globalShortcut.register('CommandOrControl+Shift+1', function() {
+		if (displays[0]) {
+			screenshot({ format: 'png', screen: displays[0].id }).then((img) => {
+				uploadImage(img);
+			});
+		}
+	});
+
+	globalShortcut.register('CommandOrControl+Shift+2', function() {
+		if (displays[1]) {
+			screenshot({ format: 'png', screen: displays[1].id }).then((img) => {
+				uploadImage(img);
+			});
+		}
+	});
+
+	globalShortcut.register('CommandOrControl+Shift+3', function() {
+		if (displays[2]) {
+			screenshot({ format: 'png', screen: displays[2].id }).then((img) => {
+				uploadImage(img);
+			});
+		}
+  });
+
+  globalShortcut.register('CommandOrControl+Shift+4', function() {
+		screenshotNode.saveScreenshot(0, 0, 100, 100, "image.png", (err) => {
+			if(err) console.log(err);
+		});
+	
+		// ioHook.once("mousedown",function(event) {
+		// 	console.log(event, getCursorLocation());\
+			// robotjs.screen.capture(x,y,width,height)
+		// })
+
+		// mouse.once("mouseup", function(event) {
+		// 	console.log(event, getCursorLocation())
+		// });
+		
+
+
+    // screenshot({ format: 'png', screen: displays[0].id }).then((img) => {
+		// 	// openCropperWindow(img);
+		// 	uploadImage(img);
+		// });
+	 });
+}
+
+
+function uploadImage(img) {
+	const date = new Date();
+	const humanReadableDate = date.getFullYear()+'-'+date.getMonth()+'-'+pad(date.getDay())+' '+date.getHours()+':'+pad(date.getMinutes());
+	var fd = {
+		name: 'Screenshot '+humanReadableDate+'.png',
+		file: img
+	} 
+
+	request.post(uploadUrl, { formData: fd}, function(err, response, body) {
+		if (err) throw err;
+		const json = JSON.parse(body);
+		json.name = fd.name;
+		
+		clipboard.writeText('https://pixeldrain.com/u/'+json.id);
+		win.webContents.send('uploadedFile', JSON.stringify(json));
+		})
+}
+
+// By god forsaken project..
+function alignAndUploadImages(images) {
+	// Load first image in
+
+	mergeImg(images).then((img) => {
+
+		console.log(img.getBuffer("image/png", err => {
+			throw err;
+		}));
+		// img.write('test.png', () => {console.log('test')})
+		// uploadImage(img.bitmap.data);
+	});
+}
+
+function pad(digit) {	return digit < 10 ? '0'+digit:digit; }
+app.on('ready', onReady)
+
+app.on('will-quit', () => {
+	globalShortcut.unregisterAll();
+});
